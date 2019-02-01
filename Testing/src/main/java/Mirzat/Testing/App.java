0f00647a85e92b61108ecff82e10ea91f447896f
mirzat <mirzat@192.168.1.82>
2019-02-01 13:37:02 -0500

    FirstProject

---
+++ Testing/src/main/java/Mirzat/Testing/App.java
@@ -0,0 +1,13 @@
+package Mirzat.Testing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
